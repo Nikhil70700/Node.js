d633040a25d3f6a931908a686a8e68e8abec2835
Nikhil Pandey <91266675+Nikhil70700@users.noreply.github.com>
2025-02-12 21:57:16 +0530

    Sync way of (FileSystem)

---
+++ Module/FS_Modules/notes.js
@@ -0,0 +1,12 @@
+//! 
+// //?This fs (File System ) module is Node.js is a core module that allows you to work with the file system ,enabling you to  read,write,update,delete,and watch files
+
+//*   fs.writeFileSync():
+
+//writes data to a file. If the file does not exist, it will be created. If the file exists, its contents will be overwritten.
+
+//!Syntax:-> fs.writeFileSync(filename, data[, options])
+
+//? filePath:The file path to write to.
+//? data: The data to be written to the file.
+//?option:Optional. Includes encoding ('utf8) mode, or flags.
