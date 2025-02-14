ed0bb96c669a789dd6de1d3b6585e9352033a91d
Nikhil Pandey <91266675+Nikhil70700@users.noreply.github.com>
2025-02-15 02:05:25 +0530

    How to create Server

---
+++ Creating_Web_Server/HttpModule/server.js
@@ -0,0 +1,26 @@
+const http=require('http');
+
+const server=http.createServer((req,res)=>{//server is Event Emitter
+if(req.url==="/"){
+    res.write("<h1>I am Nikhil Pandey</h1> ");
+    res.end();
+}
+if(req.url==="/source-code"){
+    res.setHeader("Content-Type","text/html");
+    res.write("<h1>here are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomized words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text</h1>");
+    res.end();
+}
+
+if(req.url==="/contact"){
+    res.setHeader("Content-Type","text/html")
+    res.write("<h1>Hi contact  me on (+91 7070031705)!</h1>");
+    res.end();
+}
+}); 
+
+//Creating a web server
+const PORT=4000;
+server.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
+})
+
