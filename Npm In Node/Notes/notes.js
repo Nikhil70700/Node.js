de9d297e4280be889a89420eb6da9880324669b4
Nikhil Pandey <91266675+Nikhil70700@users.noreply.github.com>
2025-02-16 03:55:57 +0530

    Playing with npm packages

---
+++ Npm In Node/Notes/notes.js
@@ -0,0 +1,9 @@
+//! Introduction to npm
+// • npm is a popular package manager which comes bundled with Node.js.
+// • It is a CLI tool used to install, update, and remove external packages.
+// • You can also create your own package and publish it on npmjs.com registry.
+// • Do not confuse npm CLI with npmjs.com, as it’s a registry where most of the packages of Node.js are saved.
+// • There are alternative Node.js package managers registry like JSR, which we will discuss in future.
+//!• Fun facts:
+// • npm shouldn’t be written in capitalized form unless you have everything as capital.
+// • npm doesn’t stand for Node Package Manager even though many people refer to it as that. It is a recursive bacronymic abbreviation for "npm is not a acronym"
