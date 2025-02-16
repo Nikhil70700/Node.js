f773d58a710aef52ee8b0b9c378948639ba1f9bb
Nikhil Pandey <91266675+Nikhil70700@users.noreply.github.com>
2025-02-16 19:58:21 +0530

    Todo App CLI

--- ProjectsNode/TodoApp/Todo.js
+++ ProjectsNode/TodoApp/Todo.js
@@ -1,9 +1,10 @@
+import chalk, { Chalk } from 'chalk';
 import readline from 'readline';
- //The readline module in Node.js is used to read input from the command line interactively. It allows handling user input from the console, making it useful for CLI applications, prompts, and interactive scripts.
+ //!The readline module in Node.js is used to read input from the command line interactively. It allows handling user input from the console, making it useful for CLI applications, prompts, and interactive scripts.
 
  const rl=readline.createInterface({
-    input:process.stdin, //to read from the standard input
-    output:process.stdout //to write to the standard output
+    input:process.stdin, // Standard input (keyboard)
+    output:process.stdout  // Standard output (console)
  })
 
  const todos=[];
@@ -12,32 +13,40 @@ import readline from 'readline';
     console.log("2: View Task:");
     console.log("3: Delete Task:");
 
-    rl.question("Choose an Option:",handleInput)
+    rl.question(chalk.bgBlueBright("Choose an Option:"),handleInput)
  }
 
  const handleInput=(option)=>{
     if(option==1){
-        rl.question("Enter the Task:",(task)=>{
+        rl.question(chalk.blue("Enter the Task:"),(task)=>{
             todos.push(task);
-            console.log("Task Added:",task);
+            console.log(("Task Added:"),chalk.blue(task));
             showMenu();
 
         })
     } else if(option==2){
         console.log("\n Your Todo Lists:");
         todos.forEach((task,index)=>{
-            console.log(`${index+1}.${task}`);
+            console.log((chalk.red.bgMagenta(`${index+1}.${task}`)));
             
         })
         showMenu();
     }else if(option==3){
-        console.log("Goodbye!");
+        console.log(chalk.red("Goodbye!"));
         rl.close();
     }else{
-        console.log("Invalid Option Choose ! Please try again");
+        console.log(chalk.red("Invalid Option Choose ! Please try again"));
     }
 
  }
 
 
  showMenu();
+
+
+ //!question():
+
+ //?The .question() method in the readline module is used to take user input in a command-line interface (CLI). It prompts the user with a question and executes a callback function with the user's response.
+ // ex:rl.question(question, callback)
+//  question: A string that is displayed to the user.
+// callback(answer): A function that receives the user's input.
